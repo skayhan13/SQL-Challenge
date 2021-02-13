723e0e9038b9afca1405bb8c684c97a3f8f89da0
Sara Kayhan <sara.kayhan@gmail.com>
2021-02-13 11:26:03 -0700

    adding ERD diagram

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -1,11 +1,11 @@
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
-FROM employees AS e LEFT JOIN salaries AS s
+FROM employees AS e LEFT JOIN salaries AS s  --created an alias with 'as s' to save time 
 ON e.emp_no=s.emp_no
 ORDER BY e.emp_no;
 
 -- 2. List first name, last name, and hire date for employees who were hired in 1986.
-SELECT * FROM employees
+SELECT first_name, last_name, hire_date FROM employees
 WHERE DATE_PART('year',hire_date) = 1986
 ORDER BY emp_no;
 
@@ -26,9 +26,8 @@ INNER JOIN departments AS d
 	ON de.dept_no=d.dept_no;
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-SELECT * FROM employees
+SELECT first_name, last_name, sex FROM employees
 WHERE first_name = 'Hercules' AND last_name like 'B%';
---add sex here 
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,73 +1,74 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Create a table for each csv you will be importing: 
+-- Create a table for each csv you will be importing: CREATE TABLE statement 
 
+-- Create departments table
 CREATE TABLE "departments" (
-    "dept_no" VARCHAR(30)   NOT NULL,
-    "dept_name" VARCHAR(300)   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY (
-        "dept_no"
-     )
+    "dept_no" VARCHAR NOT NULL,
+    "dept_name" CHAR NOT NULL, 
+    PRIMARY KEY ("dept_no")  --setting the department number as the primary key will be unique identifier 
 );
 
-CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR(30)   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_no"
-     )
-);
-
+-- Create department employees table
 CREATE TABLE "dept_emp" (
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
-        "emp_no","dept_no"
-     )
+    "emp_no" INT NOT NULL,
+    "dept_no" VARCHAR NOT NULL,
+	FOREIGN KEY ("emp_no") REFERENCES employees("emp_no"),
+	FOREIGN KEY ("dept_no") REFERENCES departments("dept_no")
 );
 
-CREATE TABLE "salaries" (
+-- Create department manager table 
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR NOT NULL,
+    "emp_no" INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- Create employees table
+CREATE TABLE "employees" ( 
     "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL,
+    "emp_title_id" VARCHAR   NOT NULL,
+    "birth_date" Date NOT NULL,
+    "first_name" VARCHAR NOT NULL,
+    "last_name" VARCHAR NOT NULL,
+    "sex" CHAR NOT NULL,
+    "hire_date" Date NOT NULL,
+	PRIMARY KEY ("emp_no")
+);
+
+-- Create salaries table
+CREATE TABLE "salaries" (
+    "emp_no" INT NOT NULL,
+    "salary" INT NOT NULL,
     CONSTRAINT "pk_salaries" PRIMARY KEY (
         "emp_no"
      )
 );
 
+-- Create titles table
 CREATE TABLE "titles" (
-    "title_id" VARCHAR   NOT NULL,
-    "title" VARCHAR(300)   NOT NULL,
+    "title_id" VARCHAR NOT NULL,
+    "title" VARCHAR NOT NULL,
     CONSTRAINT "pk_titles" PRIMARY KEY (
         "title_id"
      )
 );
 
-CREATE TABLE "employees" (   --THIS TABLE IS NOT CREATING 
-    "emp_no" INT   NOT NULL,
-    "emp_title_id" VARCHAR   NOT NULL,
-    "birth_date" Date   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "sex" CHAR   NOT NULL,
-    "hire_date" Date   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
+-- Set foreign keys after analyzing sketch; foreign keys will link tables to each other 
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+--ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+--REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
+--ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+--REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
---ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
---REFERENCES "employees" ("emp_title_id");
+ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
+REFERENCES "employees" ("emp_title_id");

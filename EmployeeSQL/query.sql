83bd518f32bfae8c9e079361c6030aea4b25a4ca
Sara Kayhan <sara.kayhan@gmail.com>
2021-02-14 14:55:40 -0700

    updated query and ERD

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -1,6 +1,6 @@
 -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
-FROM employees AS e LEFT JOIN salaries AS s  --created an alias with 'as s' to save time 
+FROM employees AS e LEFT JOIN salaries AS s  --created an alias with 'as e' and 'as s' etc. to save time 
 ON e.emp_no=s.emp_no
 ORDER BY e.emp_no;
 
@@ -27,16 +27,14 @@ INNER JOIN departments AS d
 
 -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex FROM employees
-WHERE first_name = 'Hercules' AND last_name like 'B%';
+WHERE first_name = 'Hercules' AND last_name like 'B%';  --% one form of wildcard; could also use like 'B_' potentially 
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees AS e
-LEFT JOIN dept_emp AS de
-	ON e.emp_no=de.emp_no
-INNER JOIN departments AS d
-	ON de.dept_no=d.dept_no
-WHERE d.dept_name='Sales';
+INNER JOIN dept_emp USING (emp_no)
+INNER JOIN departments AS d USING (dept_no) 
+WHERE d.dept_name = 'Sales' ORDER BY e.emp_no;
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
@@ -51,4 +49,4 @@ WHERE d.dept_name in ('Sales','Development');
 SELECT last_name, COUNT(*) AS freq_count
 FROM employees
 GROUP BY last_name
-ORDER BY freq_count DESC;
+ORDER BY freq_count DESC; --DESC is descending; ascending (ASEC) is default 
--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -1,74 +1,55 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Create a table for each csv you will be importing: CREATE TABLE statement 
+-- Critical to import these tables in the order below, since the keys make references to other tables your import will not be successful if not done in this order
 
--- Create departments table
-CREATE TABLE "departments" (
-    "dept_no" VARCHAR NOT NULL,
-    "dept_name" CHAR NOT NULL, 
-    PRIMARY KEY ("dept_no")  --setting the department number as the primary key will be unique identifier 
-);
-
--- Create department employees table
-CREATE TABLE "dept_emp" (
-    "emp_no" INT NOT NULL,
-    "dept_no" VARCHAR NOT NULL,
-	FOREIGN KEY ("emp_no") REFERENCES employees("emp_no"),
-	FOREIGN KEY ("dept_no") REFERENCES departments("dept_no")
-);
-
--- Create department manager table 
-CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR NOT NULL,
-    "emp_no" INT NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+-- Create titles table
+CREATE TABLE titles (
+    title_id VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    CONSTRAINT pk_titles PRIMARY KEY (title_id)  --setting the title_id as the primary key
 );
 
 -- Create employees table
-CREATE TABLE "employees" ( 
-    "emp_no" INT   NOT NULL,
-    "emp_title_id" VARCHAR   NOT NULL,
-    "birth_date" Date NOT NULL,
-    "first_name" VARCHAR NOT NULL,
-    "last_name" VARCHAR NOT NULL,
-    "sex" CHAR NOT NULL,
-    "hire_date" Date NOT NULL,
-	PRIMARY KEY ("emp_no")
+CREATE TABLE employees ( 
+    emp_no INT NOT NULL,
+    emp_title_id VARCHAR   NOT NULL,
+    birth_date Date NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    sex CHAR NOT NULL,
+    hire_date Date NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) references titles (title_id) --foreign key must reference the junction table
 );
 
 -- Create salaries table
-CREATE TABLE "salaries" (
-    "emp_no" INT NOT NULL,
-    "salary" INT NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY (
-        "emp_no"
-     )
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    CONSTRAINT pk_salaries PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
--- Create titles table
-CREATE TABLE "titles" (
-    "title_id" VARCHAR NOT NULL,
-    "title" VARCHAR NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
+-- Create departments table
+CREATE TABLE departments (
+    dept_no VARCHAR NOT NULL,
+    dept_name VARCHAR NOT NULL, 
+    PRIMARY KEY (dept_no)  --setting the department number as the primary key will be unique identifier 
 );
 
--- Set foreign keys after analyzing sketch; foreign keys will link tables to each other 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
+-- Create department employees table
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
---ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
---REFERENCES "employees" ("emp_no");
-
---ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
---REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "titles" ADD CONSTRAINT "fk_titles_title_id" FOREIGN KEY("title_id")
-REFERENCES "employees" ("emp_title_id");
+-- Create department manager table 
+CREATE TABLE dept_manager (
+    dept_no VARCHAR NOT NULL,
+    emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+);
